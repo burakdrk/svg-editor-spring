0ba4ce298273eaaf548f1f11f151fb97f76c9340
Burak <70870968+burakdrk@users.noreply.github.com>
2024-07-27 20:59:05 -0400

    dxfgsdgsdg

--- src/main/java/com/burakduruk/svgeditorspring/SvgEditorSpringApplication.java
+++ src/main/java/com/burakduruk/svgeditorspring/SvgEditorSpringApplication.java
@@ -1,9 +1,12 @@
 package com.burakduruk.svgeditorspring;
 
+import com.burakduruk.svgeditorspring.service.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SvgEditorSpringApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/burakduruk/svgeditorspring/controller/FileUploadController.java
+++ src/main/java/com/burakduruk/svgeditorspring/controller/FileUploadController.java
@@ -1,12 +1,19 @@
 package com.burakduruk.svgeditorspring.controller;
 
+import com.burakduruk.svgeditorspring.exception.StorageFileNotFoundException;
 import com.burakduruk.svgeditorspring.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -25,7 +32,6 @@ public class FileUploadController {
 
     @GetMapping("/get_files")
     public String listUploadedFiles(Model model) throws IOException {
-
         model.addAttribute("files", storageService.loadAll().map(
                         path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
                                 "serveFile", path.getFileName().toString()).build().toUri().toString())
@@ -34,7 +40,7 @@ public class FileUploadController {
         return "uploadForm";
     }
 
-    @GetMapping("/files/{filename:.+}")
+    @GetMapping("/uploads/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
@@ -47,7 +53,7 @@ public class FileUploadController {
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
-    @PostMapping("/")
+    @PostMapping("/upload")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
@@ -58,9 +64,9 @@ public class FileUploadController {
         return "redirect:/";
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleStorageFileNotFound(RuntimeException exc) {
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
         return ResponseEntity.notFound().build();
     }
-
 }
+
